78ca1c1525c430396b1e5871f5224e0be90d0a04
Ayrow <aymeric.pilaert@hotmail.fr>
2022-08-15 15:15:11 +0200

    created formrow component

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,56 @@
+import { useState, useEffect } from 'react';
+import { Logo, FormRow } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
+
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+  // global state and useNavigate
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={handleSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        <FormRow
+          type='name'
+          labelText='name'
+          value={values.name}
+          handleChange={handleChange}
+        />
+        <FormRow
+          type='email'
+          labelText='email'
+          value={values.email}
+          handleChange={handleChange}
+        />
+        <FormRow
+          type='password'
+          labelText='password'
+          value={values.password}
+          handleChange={handleChange}
+        />
+
+        <button type='submit' className='btn btn-block'>
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 export default Register;
