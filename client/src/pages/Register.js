92abc2207005558bf92a14b02de822dd521d4200
Ayrow <aymeric.pilaert@hotmail.fr>
2022-08-15 15:27:23 +0200

    created alert component and set up conditions on register form

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Logo, FormRow } from '../components';
+import { Logo, FormRow, Alert } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
 
 const initialState = {
@@ -7,11 +7,16 @@ const initialState = {
   email: '',
   password: '',
   isMember: true,
+  showAlert: false,
 };
 
 const Register = () => {
   const [values, setValues] = useState(initialState);
   // global state and useNavigate
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
 
   const handleChange = (e) => {
     console.log(e.target);
@@ -26,13 +31,17 @@ const Register = () => {
     <Wrapper className='full-page'>
       <form className='form' onSubmit={handleSubmit}>
         <Logo />
-        <h3>Login</h3>
-        <FormRow
-          type='name'
-          labelText='name'
-          value={values.name}
-          handleChange={handleChange}
-        />
+        <h3> {values.isMember ? 'Login' : 'Register'} </h3>
+        {values.showAlert && <Alert />}
+        {!values.isMember && (
+          <FormRow
+            type='name'
+            labelText='name'
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+
         <FormRow
           type='email'
           labelText='email'
@@ -49,6 +58,12 @@ const Register = () => {
         <button type='submit' className='btn btn-block'>
           Submit
         </button>
+        <p>
+          {values.isMember ? 'Not a member yet? ' : 'Already a member? '}
+          <button type='button' onClick={toggleMember} className='member-btn'>
+            {values.isMember ? 'Register' : 'Login'}
+          </button>
+        </p>
       </form>
     </Wrapper>
   );
