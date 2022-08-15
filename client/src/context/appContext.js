06927e433142a576733dbb239bbf2cc741b4924a
Ayrow <aymeric.pilaert@hotmail.fr>
2022-08-15 15:38:23 +0200

    set up app context and reducer

---
+++ client/src/context/appContext.js
@@ -0,0 +1,26 @@
+import { createContext } from 'react';
+import { useReducer, useContext } from 'react';
+
+import reducer from './reducer';
+
+const initialState = {
+  isLoading: false,
+  showAlert: false,
+  alertText: '',
+  alertType: '',
+};
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <AppContext.Provider value={{ ...state }}> {children} </AppContext.Provider>
+  );
+};
+
+const useAppContext = () => {
+  return useContext(AppContext);
+};
+
+export { initialState, useAppContext, AppProvider };
---
+++ client/src/context/reducer.js
@@ -0,0 +1,8 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    default:
+      throw new Error(`no such action : ${action.type}`);
+  }
+};
+
+export default reducer;
