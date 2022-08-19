f2a743159ba71870510e56217f1d30f86308c8f1
Ayrow <aymeric.pilaert@hotmail.fr>
2022-08-19 15:32:23 +0200

    setup proxy in package json

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ import errorHandlerMiddleware from './middleware/error-handler.js';
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome');
+app.get('/api/v1', (req, res) => {
+  res.json({ msg: 'API' });
 });
 
 app.use('/api/v1/auth', authRouter);
