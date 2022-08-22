5cc7f18f6d9b945a588621596dd0312dffeca848
Ayrow <aymeric.pilaert@hotmail.fr>
2022-08-22 16:37:07 +0200

    refactored register/login functions

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,12 +1,9 @@
 import {
   CLEAR_ALERT,
   DISPLAY_ALERT,
-  REGISTER_USER_BEGIN,
-  REGISTER_USER_ERROR,
-  REGISTER_USER_SUCCESS,
-  LOGIN_USER_BEGIN,
-  LOGIN_USER_ERROR,
-  LOGIN_USER_SUCCESS,
+  SETUP_USER_BEGIN,
+  SETUP_USER_SUCCESS,
+  SETUP_USER_ERROR,
 } from './actions';
 
 const reducer = (state, action) => {
@@ -25,12 +22,12 @@ const reducer = (state, action) => {
         alertType: '',
         alertText: '',
       };
-    case REGISTER_USER_BEGIN:
+    case SETUP_USER_BEGIN:
       return {
         ...state,
         isLoading: true,
       };
-    case REGISTER_USER_SUCCESS:
+    case SETUP_USER_SUCCESS:
       return {
         ...state,
         isLoading: false,
@@ -40,34 +37,9 @@ const reducer = (state, action) => {
         jobLocation: action.payload.location,
         showAlert: true,
         alertType: 'success',
-        alertText: 'User Created: Redirecting...',
+        alertText: action.payload.alertText,
       };
-    case REGISTER_USER_ERROR:
-      return {
-        ...state,
-        isLoading: false,
-        showAlert: true,
-        alertType: 'danger',
-        alertText: action.payload.msg,
-      };
-    case LOGIN_USER_BEGIN:
-      return {
-        ...state,
-        isLoading: true,
-      };
-    case LOGIN_USER_SUCCESS:
-      return {
-        ...state,
-        isLoading: false,
-        token: action.payload.token,
-        user: action.payload.user,
-        location: action.payload.location,
-        jobLocation: action.payload.location,
-        showAlert: true,
-        alertType: 'success',
-        alertText: 'Login Successful! Redirecting...',
-      };
-    case LOGIN_USER_ERROR:
+    case SETUP_USER_ERROR:
       return {
         ...state,
         isLoading: false,
