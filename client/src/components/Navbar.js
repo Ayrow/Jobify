36bbdc0088ab602dae36a3ba31d55bc22196a419
Ayrow <aymeric.pilaert@hotmail.fr>
2022-08-23 16:00:19 +0200

    setup dropdown sidebar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,40 @@
 import Wrapper from '../assets/wrappers/Navbar';
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+import Logo from './Logo';
 
 const Navbar = () => {
-  return <Wrapper>Navbar</Wrapper>;
+  const { toggleSidebar } = useAppContext();
+  return (
+    <Wrapper>
+      <div className='nav-center'>
+        <button type='button' className='toggle-btn' onClick={toggleSidebar}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className='logo-text'>dashboard</h3>
+        </div>
+        <div className='btn-container'>
+          <button
+            type='button'
+            className='btn'
+            onClick={() => console.log('show hide dropdown')}>
+            <FaUserCircle />
+            aymeric
+            <FaCaretDown />
+          </button>
+          <div className='dropdown show-dropdown'>
+            <button
+              type='button'
+              className='dropdown-btn'
+              onClick={() => console.log('logout user')}>
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 export default Navbar;
--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -4,6 +4,7 @@ import {
   SETUP_USER_BEGIN,
   SETUP_USER_SUCCESS,
   SETUP_USER_ERROR,
+  TOGGLE_SIDEBAR,
 } from './actions';
 
 const reducer = (state, action) => {
@@ -46,6 +47,11 @@ const reducer = (state, action) => {
         showAlert: true,
         alertType: 'danger',
         alertText: action.payload.msg,
+      };
+    case TOGGLE_SIDEBAR:
+      return {
+        ...state,
+        showSidebar: !state.showSidebar,
       };
     default:
       throw new Error(`no such action : ${action.type}`);
