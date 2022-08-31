9baed467171ebf2e3b4cf17e58b40a214f9a9286
Ayrow <aymeric.pilaert@hotmail.fr>
2022-08-31 15:34:50 +0200

    moved FormRowSelect to separate component and imported it in add job page

--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -1,4 +1,4 @@
-import { FormRow, Alert } from '../../components';
+import { FormRow, Alert, FormRowSelect } from '../../components';
 import { useAppContext } from '../../context/appContext';
 import Wrapper from '../../assets/wrappers/DashboardFormPage';
 
@@ -60,27 +60,22 @@ const AddJob = () => {
             handleChange={handleJobInput}
           />
         </div>
-        {/* job type */}
-        <div className='form-row'>
-          <label htmlFor='jobType' className='form-label'>
-            job type
-          </label>
-          <select
-            name='jobType'
-            value={jobType}
-            onChange={handleJobInput}
-            className='form-select'>
-            {jobTypeOptions.map((itemValue, index) => {
-              return (
-                <option key={index} value={itemValue}>
-                  {itemValue}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-
         {/* job status */}
+        <FormRowSelect
+          name='status'
+          value={status}
+          handleChange={handleJobInput}
+          list={statusOptions}
+        />
+        {/* job type */}
+        <FormRowSelect
+          name='jobType'
+          labelText='Job type'
+          value={jobType}
+          handleChange={handleJobInput}
+          list={jobTypeOptions}
+        />
+        {/* btn container */}
         <div className='btn-container'>
           <button
             type='submit'
