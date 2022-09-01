a3aefaf0c6bdbaa40612b54c39088385f9bdeb42
Ayrow <aymeric.pilaert@hotmail.fr>
2022-09-01 15:03:56 +0200

    checking permissions before updating jobs

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,7 @@
 import Job from '../models/Job.js';
 import { StatusCodes } from 'http-status-codes';
 import { BadRequestError, NotFoundError } from '../errors/index.js';
+import checkPermissions from '../utils/checkPermission.js';
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -22,22 +23,25 @@ const getAllJobs = async (req, res) => {
 const updateJob = async (req, res) => {
   const { id: jobId } = req.params;
   const { company, position } = req.body;
+
   if (!company || !position) {
     throw new BadRequestError('Please provide all values');
   }
+
   const job = await Job.findOne({ _id: jobId });
   if (!job) {
     throw new NotFoundError(`No job with id : ${jobId}`);
   }
 
   // check permission
+  checkPermissions(req.user, job.createdBy);
 
-  const updateJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+  const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
     new: true,
     runValidators: true,
   });
 
-  res.status(StatusCodes.OK).json({ updateJob });
+  res.status(StatusCodes.OK).json({ updatedJob });
 };
 
 const deleteJob = async (req, res) => {
