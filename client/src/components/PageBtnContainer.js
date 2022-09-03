899d370851ccf6f68e9f81c0d63cbd3904c4237d
Ayrow <aymeric.pilaert@hotmail.fr>
2022-09-03 15:58:59 +0200

    setup structure for pagebtncontainer

--- client/src/components/PageBtnContainer.js
+++ client/src/components/PageBtnContainer.js
@@ -1,4 +1,26 @@
+import { useAppContext } from '../context/appContext';
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi';
+import Wrapper from '../assets/wrappers/PageBtnContainer';
+
 const PageBtnContainer = () => {
-  return <div>PageBtnContainer</div>;
+  const { numOfPages, page } = useAppContext();
+
+  const nextPage = () => console.log('next page');
+
+  const prevPage = () => console.log('previous page');
+
+  return (
+    <Wrapper>
+      <button className='prev-btn' onClick={PrevPage}>
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+      <div className='btn-container'>buttons</div>
+      <button className='next-btn' onClick={nextPage}>
+        <HiChevronDoubleRight />
+        next
+      </button>
+    </Wrapper>
+  );
 };
 export default PageBtnContainer;
